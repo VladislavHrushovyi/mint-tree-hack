29fff90d98032eccca14efb0bb75ddb874e44d00
vladg <vlad.grush.99@gmail.com>
2024-08-13 19:53:25 +0300

    libs are working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { Box, Container } from '@mui/material'
+import { Box, Button, Container, Stack } from '@mui/material'
 import './App.css'
+import Grid2 from '@mui/material/Unstable_Grid2'
 
 function App() {
 
   return (
     <>
       <Container maxWidth="xl">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+        <Grid2 container spacing={2}>
+          <Grid2 md={12}>
+            <Stack direction={"row"} spacing={2}>
+              <Button variant={"outlined"}>Простий пошук</Button>
+              <Button variant={"outlined"}>Крадіжка по айді</Button>
+              <Button variant={"outlined"}>Крадіжка у лідерборда</Button>
+            </Stack>
+          </Grid2>
+        </Grid2>
       </Container>
 
     </>
