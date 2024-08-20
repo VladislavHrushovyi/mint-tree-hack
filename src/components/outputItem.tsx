2fce88f2590e2eaf9b82a373db9c8f6c69e75e53
vladg <vlad.grush.99@gmail.com>
2024-08-20 20:40:46 +0300

    trying to styling output block

--- src/components/outputItem.tsx
+++ src/components/outputItem.tsx
@@ -1,16 +1,25 @@
+import Grid2 from "@mui/material/Unstable_Grid2"
+
 interface OutputItemProps {
     info: number
+}
+
+const styles = {
+    id: {
+        fontWeight: "300",
+        color: "#333333"
+    }
 }
 
 export const OutputItem = ({info} : OutputItemProps) => {
     console.log(info)
     return (
         <>
-            <p>
-                <span>{info}.</span>
-                <span>{"id 2131313"}</span>
+            <Grid2 container fontSize={20} fontFamily={"fantasy"} fontWeight={300} spacing={0.3}>
+                <span style={styles.id}>{"id 2131313"}</span>
+                
                 <span>10230ME</span>
-            </p>
+            </Grid2>
         </>
     )
 }
