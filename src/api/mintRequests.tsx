9143b709da1efd3606aa1bc9a9205fa790abfdd5
vladg <vlad.grush.99@gmail.com>
2024-08-25 18:32:06 +0300

    add new req methods

--- src/api/mintRequests.tsx
+++ src/api/mintRequests.tsx
@@ -1,3 +1,15 @@
 export const getUserInfo = async () => {
     
 }
+
+export const getEnergyList = async () => {
+
+}
+
+export const claimMe = async () => {
+
+}
+
+export const getDetailInformation = async () => {
+    
+}
