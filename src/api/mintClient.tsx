3aaec30986ffe216161678df41b90e47ee1bc1ed
vladg <vlad.grush.99@gmail.com>
2024-08-27 21:13:36 +0300

    implement get user info request

--- src/api/mintClient.tsx
+++ src/api/mintClient.tsx
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const mintAxois = axios.create({
-    
+    baseURL: "https://www.mintchain.io/mint-forest",
+    headers: {
+        
+    }
 })
--- src/api/mintRequests.tsx
+++ src/api/mintRequests.tsx
@@ -4,8 +4,10 @@ export const mintClient = () => {
 
     const axios = mintAxois;
 
-    const getUserInfo = async () => {
-    
+    const getUserInfo = async (idTree: string) => {
+        const response  = await axios.get(`/api/tree/user-info?treeid=${idTree}`)
+
+        return response;
     }
     
     const getEnergyList = async () => {
