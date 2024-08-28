da0e1a8ec654e40805fdb8ed74fc83ea3b3b96c4
vladg <vlad.grush.99@gmail.com>
2024-08-28 20:17:40 +0300

    implement mint client

--- src/api/mintRequests.tsx
+++ src/api/mintRequests.tsx
@@ -7,25 +7,38 @@ export const mintClient = () => {
     const getUserInfo = async (idTree: string) => {
         const response  = await axios.get(`/api/tree/user-info?treeid=${idTree}`)
 
-        return response;
+        return response
     }
     
-    const getEnergyList = async () => {
-    
+    const getEnergyList = async (idUser: string) => {
+        const response = await axios.get(`/api/tree/steal/energy-list?id=${idUser}`)
+
+        return response
     }
     
-    const claimMe = async () => {
-    
+    const claimMe = async (idUser: string) => {
+        const response = await axios.get(`/api/tree/steal/claim?id=${idUser}`)
+
+        return response
     }
     
-    const getDetailInformation = async () => {
-        
+    const getDetailInformation = async (idTree: string) => {
+        const response = await axios.get(`/api/tree/activity?page=1&treeid=${idTree}`)
+
+        return response
+    }
+
+    const getTreesFromLeaderboard = async (page: string) => {
+        const response = await axios.get(`/api/tree/leaderboard?page=${page}`)
+
+        return response
     }
 
     return {
         getUserInfo,
         getEnergyList,
         claimMe,
-        getDetailInformation
+        getDetailInformation,
+        getTreesFromLeaderboard
     }
 }
