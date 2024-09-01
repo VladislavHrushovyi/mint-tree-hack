5729ad230a36107d895988ff2827391cb1d27a92
vladg <vlad.grush.99@gmail.com>
2024-09-01 20:28:05 +0300

    configuring axios headers

--- src/api/mintClient.tsx
+++ src/api/mintClient.tsx
@@ -3,6 +3,8 @@ import axios from "axios";
 export const mintAxois = axios.create({
     baseURL: "https://www.mintchain.io/mint-forest",
     headers: {
-        
+        Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        Accept: "application/json, text/plain, */*",
     }
 })
