f619ff8ee087994e48ac70ee10e4df7293f41ad6
vladg <vlad.grush.99@gmail.com>
2024-09-19 19:56:39 +0300

    starting leaderboard stelling page

--- src/pages/leaderboardSteeling.tsx
+++ src/pages/leaderboardSteeling.tsx
@@ -1,8 +1,31 @@
+import { Button } from "@mui/material"
+import Grid2 from "@mui/material/Unstable_Grid2"
+import { ProxyForm } from "../components/proxyForm"
+import { OutputArea } from "../components/outputArea"
+
+const styles = {
+    start_button : {
+        height: "100%",
+        fontSize: "5em"
+    } as React.CSSProperties
+}
+
 export const LeaderboardSteeling = () => {
-    console.log("leaderboard page steeling")
     return (
         <>
-            <h1>LEADERBOARD STEELING</h1>
+            <Grid2 container spacing={2}>
+                <Grid2 md={6} sm={12}>
+                    <Button variant="contained" style={styles.start_button} fullWidth>
+                        СТАРТУЄМ
+                    </Button>
+                </Grid2>
+                <Grid2 md={6} sm={12}>
+                    <ProxyForm />
+                </Grid2>
+                <Grid2 md={12} sm={12}>
+                    <OutputArea />
+                </Grid2>
+            </Grid2>
         </>
     )
 }
