b059fb21ed4d0d7b88ab58aa01b8590cc4639156
vladg <vlad.grush.99@gmail.com>
2024-09-20 20:14:32 +0300

    start implementing steeling choosing ids

--- src/pages/steelingPage.tsx
+++ src/pages/steelingPage.tsx
@@ -1,7 +1,22 @@
+import Grid2 from "@mui/material/Unstable_Grid2"
+import { ProxyForm } from "../components/proxyForm"
+import { OutputArea } from "../components/outputArea"
+
 export const SteelingPage = () => {
     return (
         <>
-            <h1>STEELING PAGE</h1>
+            <Grid2 container spacing={2}>
+                <Grid2 md={6}>
+                    Тіпа форма для айді дерев
+                    <p>тіпа кнопка</p>
+                </Grid2>
+                <Grid2 md={6}>
+                    <ProxyForm />
+                </Grid2>
+                <Grid2 md={12}>
+                    <OutputArea />
+                </Grid2>
+            </Grid2>
         </>
     )
 }
