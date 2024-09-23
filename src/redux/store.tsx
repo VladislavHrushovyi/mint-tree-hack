130a73fc9aa896408d3df563084524f3b9369935
vladg <vlad.grush.99@gmail.com>
2024-09-23 19:51:27 +0300

    init redux

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,1 +1,10 @@
-const store = {}
+import {configureStore} from "@reduxjs/toolkit"
+
+const store = configureStore({
+    reducer: {
+
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
